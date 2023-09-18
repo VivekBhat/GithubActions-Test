3812752a19e6ce3bdc6bb64cc9a935a55e3a4aff
Vivek Bhat <bhatvivek93@gmail.com>
2023-09-18 16:40:40 -0700

    Adding getStatus endpoint

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -4,10 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.PostConstruct;
 
 @SpringBootApplication
+@RestController // Add this annotation to mark the class as a controller
 public class App {
 
     public static void main(String[] args) {
@@ -20,6 +23,7 @@ public class App {
         log.info("Java app started");
     }
 
+    @GetMapping("/status") // Add this annotation to specify the mapping for /status
     public String getStatus() {
         return "OK";
     }
